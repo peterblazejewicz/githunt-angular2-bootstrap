c3c4b76535a44f90ff9abfd8c23610627a990d06
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-10-07 22:50:58 +0200

    Fixing modules and templates

--- src/app/feed/feed-entry.component.ts
+++ src/app/feed/feed-entry.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+export interface OnVoteEvent {
+  repoFullName: string;
+  type: string;
+}
 
 @Component({
   selector: 'githunt-feed-entry',
@@ -6,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed-entry.component.scss']
 })
 export class FeedEntryComponent implements OnInit {
-
-  constructor() { }
+  @Input() entry;
+  @Input() currentUser;
+  @Output() onVote: EventEmitter<OnVoteEvent> = new EventEmitter<OnVoteEvent>();
+  org: string;
+  repoName: string;
 
   ngOnInit() {
+    const parts = this.entry.repository.full_name.split('/');
+
+    this.org = parts[0];
+    this.repoName = parts[1];
   }
 
+  onButtonVote(type: string): void {
+    this.onVote.emit({
+      repoFullName: this.entry.repository.full_name,
+      type,
+    });
+  }
 }
--- src/app/feed/vote-buttons.component.ts
+++ src/app/feed/vote-buttons.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'githunt-vote-buttons',
   templateUrl: './vote-buttons.component.html',
   styleUrls: ['./vote-buttons.component.scss']
 })
-export class VoteButtonsComponent implements OnInit {
+export class VoteButtonsComponent {
+  @Input() canVote: boolean;
+  @Input() score: number;
+  @Input() vote: any;
+  @Output() onVote: EventEmitter<string> = new EventEmitter<string>();
 
-  constructor() { }
-
-  ngOnInit() {
+  public voteUp(): void {
+    this.submitVote('UP');
   }
 
+  public voteDown(): void {
+    this.submitVote('DOWN');
+  }
+
+  private submitVote(type: string): void {
+    if (this.canVote === true) {
+      const voteValue = {
+        UP: 1,
+        DOWN: -1,
+      }[type];
+
+      this.onVote.emit(this.vote.vote_value === voteValue ? 'CANCEL' : type);
+    }
+  }
 }
